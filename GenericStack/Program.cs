07b32e48dbe459218873239ccaf17416fc836149
vishal.Alok <you@example.com>
2021-10-15 06:29:11 +0530

    Add project files.

---
+++ GenericStack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> obj = new Stack<int>();
+            obj.Push(5);
+            obj.Push(10);
+            obj.Push(15);
+            //Printing all in LIFO
+            foreach (object o in obj)
+            {
+                Console.WriteLine(o + " ");
+            }
+            //Printing top
+            Console.WriteLine(obj.Peek());
+            //Printing top and removing
+            Console.WriteLine(obj.Pop());
+        }
+    }
+}
